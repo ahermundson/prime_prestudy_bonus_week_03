994e8bad6ca32ec921ecd5c3caa234db6f62d4e8
Alex Hermundson <ahermundson@Alexs-MacBook-Air.local>
2016-09-27 08:18:40 -0500

    Add comment explaining function process

--- scripts.js
+++ scripts.js
@@ -11,3 +11,10 @@ function subtract(num) {
 }
 
 add(multiply(subtract(2)));
+
+/*
+Both the add and multiply functions have functions being used as a parameter.
+First the subtract function will be run (in the case returning -2),
+then the multiply function will run using what subtract(2) returns as its parameter,
+then the add function runs using what multiply() returns as its parameter.
+*/
